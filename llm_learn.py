8223f1a046f8cd48f0bef8227eae957d0c530a1c
sukoonvidisha <sukoonvidisha@gmail.com>
2026-03-26 03:14:10 +0530

    Add files via upload

--- llm_learn.py
+++ llm_learn.py
@@ -1,7 +1,8 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBplggJ88G-VJRO8cdHZF9-Ee1g6NLrpt4"
+import streamlit as st
+os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)
 
